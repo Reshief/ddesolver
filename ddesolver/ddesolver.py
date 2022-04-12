9046a5aa493c97e541ebda2f5917865621f3f88c
Reshief <reshief@gmx.net>
2022-04-12 17:18:11 +0200

    Move example code to example code file

--- ddesolver/ddesolver.py
+++ ddesolver/ddesolver.py
@@ -116,35 +116,6 @@ def solve_dde(func, generator, tt, fargs=None):
     fargs
       Additional arguments to be passed to parameter ``func``, if any.
 
-
-    Examples
-    ---------
-    
-    We will solve the delayed Lotka-Volterra system defined as
-    
-        For t < 0:
-        x(t) = 1+t
-        y(t) = 2-t
-    
-        For t >= 0:
-        dx/dt =  0.5* ( 1- y(t-d) )
-        dy/dt = -0.5* ( 1- x(t-d) )
-    
-    The delay ``d`` is a tunable parameter of the model.
-
-    >>> import numpy as np
-    >>> from ddesolver import solve_dde
-    >>> 
-    >>> def model(XY,t,d):
-    >>>     x, y = XY(t)
-    >>>     xd, yd = XY(t-d)
-    >>>     return np.array([0.5*x*(1-yd), -0.5*y*(1-xd)])
-    >>> 
-    >>> generator = lambda t : np.array([1+t,2-t]) # 'history' at t<0
-    >>> tt = np.linspace(0,30,20000) # times for integration
-    >>> d = 0.5 # set parameter d 
-    >>> yy = solve_dde(model,generator,tt,fargs=(d,)) # solve the DDE !
-     
     """
 
     dde_ = dde(func)
--- examples/lotka.py
+++ examples/lotka.py
@@ -1,5 +1,19 @@
 """ Lotka volterra DDE """
 
+
+"""
+    We will solve the delayed Lotka-Volterra system defined as
+    
+        For t < 0:
+        x(t) = 1+t
+        y(t) = 2-t
+    
+        For t >= 0:
+        dx/dt =  0.5* ( 1- y(t-d) )
+        dy/dt = -0.5* ( 1- x(t-d) )
+    
+    The delay ``d`` is a tunable parameter of the model.
+"""
 from pylab import array, linspace, subplots
 from ddesolver import solve_dde
 
@@ -15,6 +29,7 @@ tt = linspace(2, 30, 20000)
 
 fig, ax = subplots(1, figsize=(4, 4))
 
+# The parameter d is configurable
 for d in [0, 0.2]:
     print("Computing for d=%.02f" % d)
     yy = solve_dde(model, g, tt, fargs=(d,))
